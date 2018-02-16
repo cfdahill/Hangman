30f7cd94b45be0da32ae3274da8742586b1ec322
cfdahill <cfdahill@gmail.com>
2018-02-15 19:22:14 -0700

    Got hangman mostly working, still need to sylize it

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,5 @@
 //array of object names to be able to pick a random object
-var wordBank = [skywalker, spiderman];
+var wordBank = ["luke skywalker", "spider man"];
 
 //Objects are the words, will add more later
 var skywalker = {
@@ -16,36 +16,109 @@ var spiderman = {
 };
 
 var dashString = "-------------------------------------";
-var newDashString; //this will make the dashString the same length as the answer
+var newDashString = ""; //this will make the dashString the same length as the answer
+var obj = "";
+var wrongGuess = [];
+var newDashArray = [];
+var partialAnswer = "";
+
+var wins = 0;
+var lives = 6;
+
+var wrong = function () {
+    lives--;
+    document.getElementById("strikes").innerHTML = lives;
+};
+
+var gameOver = function () {
+    lives = 6;
+    document.getElementById("strikes").innerHTML = lives;
+    document.getElementById("wrong").innerHTML = "";
+    document.getElementById("previousAnswer").innerHTML = obj;
+    word();
+    space();
+    //document.getElementById("currentGame").innerHTML = newDashString;
+};
+
+var victory = function () {
+    wins++;
+    gameOver();
+    document.getElementById("wins").innerHTML = wins;
+};
+
+//automatically replace any - with spaces, let's get everything else working first
+function space(){
+    if (obj.indexOf(" ") !== -1) {
+        for (i = 0; i < obj.length; i++) {
+            if (obj.charAt(i) === " ") {
+                newDashArray.splice(i, 1, " ");
+            }
+        }
+
+        partialAnswer = newDashArray.join("");
+        document.getElementById("currentGame").innerHTML = partialAnswer;
+        }
+    }
 
 //pick a random word and create a dashString of the same length
-function word(){
-    var obj = wordbank[Math.floor(Math.random() * wordBank.length)];
-    var newDashString = dashString.slice(0,obj.answer.length);
-    console.log(obj.answer);
-    console.log(newDashString);
+function word() {
+    obj = wordBank[Math.floor(Math.random() * wordBank.length)];
+    newDashString = dashString.slice(0, obj.length);
+    newDashArray = newDashString.split("");
+
+    console.log(obj);
+    console.log(newDashArray);
 }
 
-//automatically replace any - with spaces
-function space(){}
-    if (obj.answer.indexOf(" ") !== -1) {
+word();
 
+
+   
+
+
+document.onkeyup = function (event) {
+    var guess = event.key;
+    var objArray = obj.split("");
+    var letter = obj.indexOf(guess);
+    document.getElementById("currentGame").innerHTML = newDashString;
+space();
+
+    //add wrong guesses to the wrong guess div first time, take off point first time
+    if (letter === -1) {
+        if ((wrongGuess.indexOf(guess) === -1)) {
+            wrongGuess.push(guess);
+            //console.log(wrongGuess);
+            document.getElementById("wrong").innerHTML = wrongGuess;
+            wrong();
+            if (lives === 0) {
+                gameOver();
+            }
+        }
+    }
+    //make letter appear
+    else if (letter !== -1) {
+        for (i = 0; i < obj.length; i++) {
+            if (guess === objArray[i]) {
+                newDashArray.splice(i, 1, guess);
+            }
+        }
+
+        partialAnswer = newDashArray.join("");
+        document.getElementById("currentGame").innerHTML = partialAnswer;
+        }
+        if (partialAnswer === obj) {
+            victory();
     }
 
-    for (i=0; i<obj.answer.length; i++){
-        
-    }
-}
-document.onkeyup = function(event) {
 
 }
 
 /* Pseudocode
 indexOf() will be our friend
 +++Create objects with word, universe, picture, alt for picks
-Have computer pick a random object 
++++Have computer pick a random object 
     (might need to put object names into an array)
-Computer determines length of string
++++Computer determines length of string
 Computer displays ___ for each letter and shows any spaces as appropriate
 Computer displays background image based off of universe as clue
 Let person hit a key
