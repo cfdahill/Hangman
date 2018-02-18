5ea73f489825d75a63ca6734fae9464d19c95d29
cfdahill <cfdahill@gmail.com>
2018-02-17 19:43:40 -0700

    finished version of hangman and unfinished version of psychic

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,19 +1,113 @@
-//array of object names to be able to pick a random object
-var wordBank = ["luke skywalker", "spider man"];
-
-//Objects are the words, will add more later
+/* universe key for background image and soundbites: 0: star wars, 1: marvel, 2: dc comics, 3: paw patrol*/
 var skywalker = {
     answer: "luke skywalker",
-    universe: "starWars",
-    picture: "https://cdn.pastemagazine.com/www/articles/Once-and-Future-Luke-header.jpg",
+    universe: 0,
+    picture: "./assets/images/luke.jpg",
     alt: "Luke Skywalker"
 };
+var princess = {
+    answer: "princess leia",
+    universe: 0,
+    picture: "./assets/images/princessleia.jpg",
+    alt: "Princess Leia"
+};
+var hanSolo = {
+    answer: "han solo",
+    universe: 0,
+    picture: "./assets/images/hanSolo.jpg",
+    alt: "Han Solo"
+};
+var darth = {
+    answer: "darth vader",
+    universe: 0,
+    picture: "./assets/images/vader.jpg",
+    alt: "Darth Vader"
+};
+var yoda = {
+    answer: "yoda",
+    universe: 0,
+    picture: "./assets/images/yoda.jpeg",
+    alt: "Yoda"
+};
+var thor = {
+    answer: "thor",
+    universe: 1,
+    picture: "./assets/images/thor.jpg",
+    alt: "Thor"
+};
 var spiderman = {
-    answer: "spiderman",
-    universe: "marvel",
-    picture: "https://pmcvariety.files.wordpress.com/2017/02/spidermanavengersdowney.jpg?w=1000&h=563&crop=1",
+    answer: "spiderman ",
+    universe: 1,
+    picture: "./assets/images/spiderman.jpg",
     alt: "Spiderman"
 };
+var ironman = {
+    answer: "ironman",
+    universe: 1,
+    picture: "./assets/images/ironman.jpg",
+    alt: "Ironman"
+};
+var captain = {
+    answer: "captain america",
+    universe: 1,
+    picture: "./assets/images/captain.jpg",
+    alt: "Captain America"
+};
+var batman = {
+    answer: "batman",
+    universe: 2,
+    picture: "./assets/images/batman.jpg",
+    alt: "Batman"
+};
+var superman = {
+    answer: "superman",
+    universe: 2,
+    picture: "./assets/images/superman.jpg",
+    alt: "Superman"
+};
+var wonder = {
+    answer: "wonder woman",
+    universe: 2,
+    picture: "./assets/images/wonder.jpg",
+    alt: "Wonder Woman"
+};
+var marshall = {
+    answer: "marshall",
+    universe: 3,
+    picture: "./assets/images/marshall.jpg",
+    alt: "Marshall"
+};
+var skye = {
+    answer: "skye",
+    universe: 3,
+    picture: "./assets/images/skye.jpg",
+    alt: "Skye"
+};
+var chase = {
+    answer: "chase",
+    universe: 3,
+    picture: "./assets/images/chase.jpg",
+    alt: "Chase"
+};
+var rocky = {
+    answer: "rocky",
+    universe: 3,
+    picture: "./assets/images/rocky.jpg",
+    alt: "Rocky"
+};
+var rubble = {
+    answer: "rubble",
+    universe: 3,
+    picture: "./assets/images/rubble.jpg",
+    alt: "rubble"
+};
+
+//array of object names to be able to pick a random object
+var wordBank = [skywalker, princess, hanSolo, darth, yoda, thor, spiderman, ironman, captain, batman, superman, wonder, marshall, skye, chase, rocky, rubble];
+//the background image is based on  object.universe
+var background = ["./assets/images/starWars.jpg", "./assets/images/avengers.jpeg", "./assets/images/dc.jpg", "./assets/images/pawPatrol.jpg"];
+//audio files, based on object.universe
+var audio = ["./assets/mp3s/chewy_roar.mp3", "./assets/mp3s/smash.mp3", "./assets/mp3s/batman.mp3", "./assets/mp3s/dog3.mp3"]
 
 var dashString = "-------------------------------------";
 var newDashString = ""; //this will make the dashString the same length as the answer
@@ -21,33 +115,49 @@ var obj = "";
 var wrongGuess = [];
 var newDashArray = [];
 var partialAnswer = "";
+//These will all turn the object.keys into variables
+var objUniverse;
+var objPicture;
+var objAlt;
+var objAudio;
 
 var wins = 0;
 var lives = 6;
 
+//Takes away a life
 var wrong = function () {
     lives--;
     document.getElementById("strikes").innerHTML = lives;
 };
 
+//Starts a new game and resets lives and wrong guesses
 var gameOver = function () {
     lives = 6;
+    wrongGuess = [];
     document.getElementById("strikes").innerHTML = lives;
     document.getElementById("wrong").innerHTML = "";
     document.getElementById("previousAnswer").innerHTML = obj;
+    document.getElementById("previousImg").innerHTML = ('<img src= "' + objPicture + '" alt= "' + objAlt + '">')
     word();
+    backgroundImg();
+    document.getElementById("currentGame").innerHTML = newDashString;
     space();
-    //document.getElementById("currentGame").innerHTML = newDashString;
 };
 
+var song = function () {
+    var sound = new Audio(objAudio)
+    sound.play()
+}
+//Starts a new game and gives player a point
 var victory = function () {
     wins++;
+    song();
     gameOver();
     document.getElementById("wins").innerHTML = wins;
 };
 
-//automatically replace any - with spaces, let's get everything else working first
-function space(){
+//automatically replace any - with spaces
+function space() {
     if (obj.indexOf(" ") !== -1) {
         for (i = 0; i < obj.length; i++) {
             if (obj.charAt(i) === " ") {
@@ -57,31 +167,38 @@ function space(){
 
         partialAnswer = newDashArray.join("");
         document.getElementById("currentGame").innerHTML = partialAnswer;
-        }
     }
+}
 
 //pick a random word and create a dashString of the same length
 function word() {
-    obj = wordBank[Math.floor(Math.random() * wordBank.length)];
+    var object = wordBank[Math.floor(Math.random() * wordBank.length)];
+    obj = object.answer;
+    objUniverse = background[object.universe];
+    objAudio = audio[object.universe];
+    objPicture = object.picture;
+    objAlt = object.alt;
     newDashString = dashString.slice(0, obj.length);
     newDashArray = newDashString.split("");
+    console.log(objUniverse);
+    //console.log(obj);
+}
 
-    console.log(obj);
-    console.log(newDashArray);
+function backgroundImg() {
+    document.body.style.backgroundImage = 'url(' + objUniverse + ')';
 }
 
 word();
 
-
-   
-
-
+//responding to keystrokes
 document.onkeyup = function (event) {
+    
+    backgroundImg();
     var guess = event.key;
     var objArray = obj.split("");
     var letter = obj.indexOf(guess);
     document.getElementById("currentGame").innerHTML = newDashString;
-space();
+    space();
 
     //add wrong guesses to the wrong guess div first time, take off point first time
     if (letter === -1) {
@@ -91,6 +208,8 @@ space();
             document.getElementById("wrong").innerHTML = wrongGuess;
             wrong();
             if (lives === 0) {
+                document.getElementById("previousAnswer").innerHTML = obj;
+
                 gameOver();
             }
         }
@@ -105,9 +224,9 @@ space();
 
         partialAnswer = newDashArray.join("");
         document.getElementById("currentGame").innerHTML = partialAnswer;
-        }
-        if (partialAnswer === obj) {
-            victory();
+    }
+    if (partialAnswer === obj) {
+        victory();
     }
 
 
